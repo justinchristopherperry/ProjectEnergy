f0772c5e65b1e64b4dfa9245583515bc331bdfe7
Valerie Roberts <vgr3@duke.edu>
2019-12-01 22:01:53 -0500

    added price range to matchmaker

--- matchmaker/forms.py
+++ matchmaker/forms.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.forms import ModelForm
 from .models import Seller
 
@@ -13,3 +14,28 @@ class SellerForm(ModelForm):
                     'country_name':'Country',
                     'total_produced_2018_Gwh':'Electricity produced in 2018 (Gwh)',
                     'price_per_kwh':'Price per Kwh'}
+
+class MatchForm(forms.Form):
+	minPrice = forms.FloatField(label='Minimum price per kwh:', required=False, min_value=0.0)
+	maxPrice = forms.FloatField(label='Maximum price per kwh:', required=False, min_value=0.001)
+
+	def clean_match_form(self):
+		minP = self.cleaned_data['minPrice']
+		print(f"in form:{type(minP)}")
+		maxP = self.cleaned_data['maxPrice']
+		if minP is None:
+			minP = 0.0
+		if maxP is None:
+			maxP = 1000000
+		if minP < 0:
+			raise ValidationError(_('Invalid price - price is below 0'))
+		if maxP <= 0:
+			raise ValidationError(_('Invalid price - max price must be ablow 0'))
+
+		check = {
+			'minPrice': minP,
+			'maxPrice': maxP
+		}
+		print(f"in form2:{type(check['minPrice'])}")
+		return check
+    	
--- matchmaker/views.py
+++ matchmaker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Seller
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from .forms import SellerForm
+from .forms import SellerForm, MatchForm
 
 def home(request):
     sellers_in_egypt = Seller.objects.filter(country_name='Egypt')
@@ -13,8 +13,20 @@ def test(request):
     return render(request, 'test.html', {'sellers_in_egypt': sellers_in_egypt})
 
 def matchmaker(request):
-	#(prices, countries, sortBy) = get_sql_params() - i'm not sure what to do with this
-	prices = [.01, .05]
+	#(prices, countries, sortBy) = get_sql_params() - i'm not sure what to do with this 
+	prices = []
+	if request.method == 'POST':
+		info = MatchForm(request.POST)
+		if info.is_valid():
+			info = info.clean_match_form()
+			prices = [info['minPrice'], info['maxPrice']]
+	else: 
+		info = MatchForm()
+
+
+	#prices = [.01, .05]
+	if len(prices) > 0:
+		print(f"{prices}, {type(prices[0])}")
 	countries = ['Benin', 'Angola']
 	sortBy = ['price_per_kwh','country_name']
 
@@ -30,7 +42,7 @@ def matchmaker(request):
 	if len(sortBy) == 2:
 		table = table.order_by(sortBy[0], sortBy[1])
 
-	return render(request, 'matchmaker.html', {'matches': table})
+	return render(request, 'matchmaker.html', {'matches': table, 'info': info})
 
 def newseller(request):
 	if request.method == 'POST':
